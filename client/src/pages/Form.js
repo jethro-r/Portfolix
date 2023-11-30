22526a422be44524dc9748fedd56ef7ea521f30e
Jethro <readjethro@gmail.com>
2023-11-30 15:07:53 +1300

    progress #2, form css

--- client/src/pages/Form.js
+++ client/src/pages/Form.js
@@ -17,13 +17,13 @@ export const Form = () => {
     });
   };
 
-  const [page, setPage] = useState(0);
+  const [page, setPage] = useState(1);
   const conditionalComponent = () => {
     switch (page) {
       case 0:
         return <Tab0 id="0" formData={formData} />;
       case 1:
-        return <Tab1 id="1" formData={formData} />;
+        return <Tab0 id="1" formData={formData} />;
       case 2:
         return <Tab2 id="2" formData={formData} />;
       case 3:
@@ -46,7 +46,7 @@ export const Form = () => {
 
   const FormSectionHeader = (props) => {
     return (
-      <div class="w-[1022px] h-[172px] flex flex-col items-center justify-center bg-cyan-900 rounded-lg">
+      <div class="w-full h-[172px] flex flex-col items-center justify-center bg-cyan-900 rounded-lg">
         <div class="text-white text-[40px] font-normal">
           Template Name - Form
           <br />
@@ -58,23 +58,28 @@ export const Form = () => {
 
   const FormSectionFooter = () => {
     return (
-      <div>
-        <button onClick={handleSubmit}></button>
-        {page > 0 && <button onClick={() => setPage(page - 1)}>Back</button>}
+      <div className="w-full self-end">
+        <div class="flex justify-between">
+          {page > 0 && (
+            <button
+              onClick={() => setPage(page - 1)}
+              class="w-[126px] h-14 bg-sky-700 rounded-[10px] text-center text-white text-xl font-bold"
+            >
+              Back
+            </button>
+          )}
 
-        <div class="w-[1022px] h-[22px] bg-zinc-400 rounded-[99px]">
-          <div class="w-[170px] h-full bg-green-600 rounded-[99px]"></div>
+          <button class="w-[126px] h-14 bg-sky-700 rounded-[10px] text-center text-white text-xl font-bold">
+            {page === 0 || page === 7 ? "Next" : "Submit"}
+          </button>
         </div>
-        <div class="w-[126px] h-14 bg-sky-700 rounded-[10px] text-center text-white text-xl font-bold">
-          BACK
-        </div>
-        <div class="w-[126px] h-14 bg-sky-700 rounded-[10px] text-center text-white text-xl font-bold">
-        {page === 0 || page === 7 ? "Next" : "Submit"}
-        </div>
-
-
-        <div class="w-[79px] h-8 left-[1471px] top-[983px] absolute text-center text-neutral-500 text-[25px] font-bold tracking-[2.50px]">
-          {page}/7
+        <div class="flex justify-center items-center mt-4">
+          <div class="flex-grow h-[22px] bg-zinc-400 rounded-[99px]">
+            <div class="w-[170px] h-full bg-green-600 rounded-[99px]"></div>
+          </div>
+          <div class="h-8 mx-4 text-justify text-neutral-500 text-[25px] font-bold">
+            {page}/7
+          </div>
         </div>
       </div>
     );
@@ -82,61 +87,63 @@ export const Form = () => {
 
   const Tab0 = ({ formData, setFormData }) => {
     return (
-      <div>
+      <div class="flex flex-col h-full items-center justify-between px-32">
         <FormSectionHeader />
         <div>
-          <div class="flex flex-col justify-center">
-            <div>First Name: </div>
-            <input
-              onChange={handleFormState}
-              value={formData.name}
-              placeholder="John Doe"
-              required
-              class="w-[800px] h-[120px] bg-slate-500 rounded-[10px] border border-black"
-            ></input>
+          <div>
+            <div class="flex flex-col justify-center">
+              <div>First Name: </div>
+              <input
+                onChange={handleFormState}
+                value={formData.name}
+                placeholder="John Doe"
+                required
+                class="w-[800px] h-[120px] bg-slate-500 rounded-[10px] border border-black"
+              ></input>
+            </div>
+          </div>
+          <FormInput
+            props={{ title: "Last Name" }}
+            formData={formData}
+            handleFormState={handleFormState}
+          />
+          <FormInput
+            props={{ title: "Email Address" }}
+            formData={formData}
+            handleFormState={handleFormState}
+          />
+          <FormInput
+            props={{ title: "Address" }}
+            formData={formData}
+            handleFormState={handleFormState}
+          />
+          <FormInput
+            props={{ title: "Phone Number" }}
+            formData={formData}
+            handleFormState={handleFormState}
+          />
+          <FormInput
+            props={{ title: "Website Link" }}
+            formData={formData}
+            handleFormState={handleFormState}
+          />
+          <div class="w-[200px] h-[230px] left-[1176px] top-[428px] absolute">
+            <div class="w-[200px] h-[200px] left-0 top-[30px] absolute bg-zinc-300 rounded-[400px] border border-black"></div>
+            <div class="w-[200px] h-[30px] text-center text-zinc-800 text-sm font-normal">
+              Profile Picture
+            </div>
+            <div class="w-[200px] h-[30px] left-0 top-[115px] absolute text-center text-zinc-800 text-sm font-normal">
+              Upload
+            </div>
           </div>
         </div>
-        <FormInput
-          props={{ title: "Last Name" }}
-          formData={formData}
-          handleFormState={handleFormState}
-        />
-        <FormInput
-          props={{ title: "Email Address" }}
-          formData={formData}
-          handleFormState={handleFormState}
-        />
-        <FormInput
-          props={{ title: "Address" }}
-          formData={formData}
-          handleFormState={handleFormState}
-        />
-        <FormInput
-          props={{ title: "Phone Number" }}
-          formData={formData}
-          handleFormState={handleFormState}
-        />
-        <FormInput
-          props={{ title: "Website Link" }}
-          formData={formData}
-          handleFormState={handleFormState}
-        />
-        <div class="w-[200px] h-[230px] left-[1176px] top-[428px] absolute">
-          <div class="w-[200px] h-[200px] left-0 top-[30px] absolute bg-zinc-300 rounded-[400px] border border-black"></div>
-          <div class="w-[200px] h-[30px] text-center text-zinc-800 text-sm font-normal">
-            Profile Picture
-          </div>
-          <div class="w-[200px] h-[30px] left-0 top-[115px] absolute text-center text-zinc-800 text-sm font-normal">
-            Upload
-          </div>
-        </div>
-        <FormSectionFooter />
+        <FormSectionFooter class="self-center" />
       </div>
     );
   };
 
   const Tab1 = ({ formData, setFormData }) => {
-    return <div>Tab1</div>;
+    return <div></div>;
   };
   const Tab2 = ({ formData, setFormData }) => {
     return (
@@ -164,6 +171,7 @@ export const Form = () => {
   return (
     <div class="w-full relative flex flex-col">
       <Navbar />
+
       {conditionalComponent()}
     </div>
   );

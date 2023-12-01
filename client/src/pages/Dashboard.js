484f55794b9a43f754d6bf0ad3d8ae75341d83d6
pyppeer <pmcc211@mywhitecliffe.com>
2023-12-01 23:17:45 +1300

    Dashboard page
    Made Dashboard responsive.
    Installed the icons for the containers.

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,67 +1,113 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import { FaEdit, FaTrash } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
-export default function Dashboard() {
+const Dashboard = () => {
+  const containerStyles = 'bg-white mx-auto px-4 py-8 '; 
+  const rowStyles = 'bg-zinc-300 p-6 my-4 relative flex items-center justify-between shadow-xl border border-black';
+  const textContainerStyles = 'flex-1 pl-6';
+  const imageContainerStyles = 'flex-1 h-auto max-w-[40%]';
+  const buttonStyles = 'absolute bottom-2 right-2 flex space-x-2';
+  const newButtonStyles = 'absolute top-1 right-4 text-2xl text-black py-2 px-4';
+
   return (
     <div className="bg-white">
     <Navbar />
 
-    <div className="relative bg-blue-300 bg-opacity-40 h-[35vh] md:h-[25vh] z-10"> 
-      <div className="absolute inset-0 flex items-center justify-center">
-       <div className="text-center text-zinc-800 text-4xl md:text-6xl font-normal font-['Russo One']">DASHBOARD</div>
+    <div className="relative">
+        <div className="bg-blue-300 bg-opacity-40 h-[35vh] md:h-[25vh] z-10">
+          <div className="absolute inset-0 flex items-center justify-center">
+            <div className="text-center text-zinc-800 text-4xl md:text-6xl font-normal font-['Russo One']">
+              DASHBOARD
+            </div>
+          </div>
+        </div>
+      </div>
+     
+      <div className="relative">
+        <Link to="/templates" className={newButtonStyles}>NEW+</Link>
+      </div>
+
+      <div className={containerStyles}>
+      <div className={rowStyles}>
+        <div className={imageContainerStyles}>
+          <img
+            className="w-full h-auto"
+            src="http://localhost:3000/images/dashboard-image-1.png"
+            alt=""
+          />
+      <div className={buttonStyles}>
+        <button className="bg-transparent border-2 border-black text-black rounded-full p-3 hover:bg-white-500 hover:text-white">
+          <FaEdit className="text-lg md:text-xl lg:text-2xl" />
+        </button>
+        <button className="bg-transparent border-2 border-black text-black rounded-full p-3 hover:bg-white-500 hover:text-white">
+          <FaTrash className="text-lg md:text-xl lg:text-2xl" />
+        </button>
+      </div>
+        </div>
+        <div className={textContainerStyles}>
+          <h2 className="text-2xl font-bold mb-2">Colourful template</h2>
+          <p className="text-lg">A bright and colourful design created by @user</p>
+        </div>
       </div>
     </div>
 
-    <div class="left-[1665px] top-[412px] absolute text-center text-black text-[66px] font-normal font-['Russo One']">NEW +</div>
-    
-    <div class="w-[1832px] h-[424px] left-[42px] top-[979px] absolute">
-        <div class="w-[1828px] h-[424px] left-[4px] top-0 absolute bg-zinc-300 border border-black"></div>
-        <img class="w-[672px] h-[424px] left-0 top-0 absolute border border-black" 
-        src="http://localhost:3000/images/dashboard-image-1.png" 
-        alt=''/>
-        <div class="w-[145px] h-[63.01px] left-[1659px] top-[342px] absolute">
-            <div class="w-[64.88px] h-[63.01px] left-0 top-0 absolute bg-zinc-300 rounded-full border-4 border-black"></div>
-            <div class="w-[64.88px] h-[63.01px] left-[80.11px] top-0 absolute bg-zinc-300 rounded-full border-4 border-black"></div>
-            <div class="w-[35.69px] h-[35.69px] left-[94.46px] top-[13.10px] absolute">
-            </div>
+
+
+    <div className={containerStyles}>
+      <div className={rowStyles}>
+        <div className={imageContainerStyles}>
+          <img
+            className="w-full h-auto"
+            src="http://localhost:3000/images/dashboard-image-2.png"
+            alt=""
+          />
+          <div className={buttonStyles}>
+            <button className="bg-transparent border-2 border-black text-black rounded-full p-3 hover:bg-white-500 hover:text-white">
+              <FaEdit className="text-2xl"/>
+            </button>
+            <button className="bg-transparent border-2 border-black text-black rounded-full p-3 hover:bg-white-500 hover:text-white">
+              <FaTrash className="text-2xl"/>
+            </button>
+          </div>
         </div>
-        <div class="left-[729px] top-[176px] absolute text-black text-3xl font-normal font-['Russo One']">A simple and minimal design created by @user</div>
-        <div class="left-[729px] top-[89px] absolute text-black text-[40px] font-normal font-['Russo One']">Blue and white template</div>
+        <div className={textContainerStyles}>
+          <h2 className="text-2xl font-bold mb-2">Blue and white template</h2>
+          <p className="text-lg">A simple and minimal design created by @user</p>
+        </div>
+      </div>
     </div>
-    
-    <div class="w-[1832px] h-[424px] left-[46px] top-[511px] absolute">
-        <div class="w-[1832px] h-[424px] left-0 top-0 absolute bg-zinc-300 border border-black"></div>
-        <img class="w-[672px] h-[424px] left-0 top-0 absolute shadow" 
-        src="http://localhost:3000/images/dashboard-image-2.png" 
-        alt=''/>
-        <div class="w-[145px] h-[63.01px] left-[1659px] top-[342px] absolute">
-            <div class="w-[64.88px] h-[63.01px] left-0 top-0 absolute bg-zinc-300 rounded-full border-4 border-black"></div>
-            <div class="w-[64.88px] h-[63.01px] left-[80.11px] top-0 absolute bg-zinc-300 rounded-full border-4 border-black"></div>
-            <div class="w-[35.69px] h-[35.69px] left-[94.46px] top-[13.10px] absolute">
-            </div>
+
+    <div className={containerStyles}>
+      <div className={rowStyles}>
+        <div className={imageContainerStyles}>
+          <img
+            className="w-full h-auto"
+            src="http://localhost:3000/images/dashboard-image-3.png"
+            alt=""
+          />
+          <div className={buttonStyles}>
+            <button className="bg-transparent border-2 border-black text-black rounded-full p-3 hover:bg-white-500 hover:text-white">
+              <FaEdit className="text-2xl"/>
+            </button>
+            <button className="bg-transparent border-2 border-black text-black rounded-full p-3 hover:bg-white-500 hover:text-white">
+              <FaTrash className="text-2xl"/>
+            </button>
+          </div>
         </div>
-        <div class="left-[725px] top-[176px] absolute text-black text-3xl font-normal font-['Russo One']">A bright and colourful design created by @user</div>
-        <div class="w-[483px] left-[725px] top-[95px] absolute text-black text-[40px] font-normal font-['Russo One']">Colourful template</div>
-    </div>
-    
-    <div class="w-[1832px] h-[424px] left-[42px] top-[1447px] absolute">
-        <div class="w-[1828px] h-[424px] left-[4px] top-0 absolute bg-zinc-300 border border-black"></div>
-        <img class="w-[672px] h-[423px] left-0 top-[1px] absolute" 
-        src="http://localhost:3000/images/dashboard-image-3.png" 
-        alt=''/>
-        <div class="w-[145px] h-[63.01px] left-[1659px] top-[342px] absolute">
-            <div class="w-[64.88px] h-[63.01px] left-0 top-0 absolute bg-zinc-300 rounded-full border-4 border-black"></div>
-            <div class="w-[64.88px] h-[63.01px] left-[80.11px] top-0 absolute bg-zinc-300 rounded-full border-4 border-black"></div>
-            <div class="w-[35.69px] h-[35.69px] left-[94.46px] top-[13.10px] absolute">
-            </div>
+        <div className={textContainerStyles}>
+          <h2 className="text-2xl font-bold mb-2">Complex template</h2>
+          <p className="text-lg">A complex design created by @user</p>
         </div>
-        <div class="w-[483px] left-[729px] top-[95px] absolute text-black text-[40px] font-normal font-['Russo One']">Complex template</div>
-        <div class="left-[729px] top-[176px] absolute text-black text-3xl font-normal font-['Russo One']">A complex design created by @user</div>
+      </div>
     </div>
 
 
-    
+    <Footer />
     </div>
-  )
-}
+  );
+};
+
+export default Dashboard;
